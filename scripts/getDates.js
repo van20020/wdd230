47287966012f47ccefa627dad75cb94cfac77a22
van20020 <van20020@byui.edu>
2024-05-01 11:39:03 -0600

    last updates

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,5 +1,2 @@
-// Select the paragraph element by its id
 let lastModified = document.getElementById("lastModified");
-
-// Update the inner text of the paragraph with the last modified date
 lastModified.innerText += document.lastModified;
