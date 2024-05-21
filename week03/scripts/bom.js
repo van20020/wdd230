1c4d274e149524a4dc43ec80d60033638b377fc9
van20020 <van20020@byui.edu>
2024-05-21 10:20:57 -0600

    changed favicon pathing. updated homepage with links

--- week03/scripts/bom.js
+++ week03/scripts/bom.js
@@ -1,57 +1,61 @@
-const input = document.querySelector('#favchap');
+// const input = document.querySelector('#favchap');
 
-const button = document.querySelector('button');
+// const button = document.querySelector('button');
 
-const list = document.querySelector('#list');
+// const list = document.querySelector('#list');
 
 
-// Arrow Syntax
-button.addEventListener('click', () => {
- if (input.value) {
-    // Create an li element
-    const li = document.createElement('li');
+// // Arrow Syntax
+// button.addEventListener('click', () => {
+//  if (input.value) {
+//     // Create an li element
+//     const li = document.createElement('li');
 
-    //Create a delete button
-    const deleteButton = document.createElement('button');
+//     //Create a delete button
+//     const deleteButton = document.createElement('button');
 
-    //Populate the li elements textContent or innerHTML with the input value
-    li.textContent = input.value;
+//     //Populate the li elements textContent or innerHTML with the input value
+//     li.textContent = input.value;
 
-    //Populate the button textContent with a ❌
-    deleteButton.textContent = '❌';
+//     //Populate the button textContent with a ❌
+//     deleteButton.textContent = '❌';
 
-    //Append the li element with the delete button
-    li.appendChild(deleteButton);
+//     //Append the li element with the delete button
+//     li.appendChild(deleteButton);
 
-    //Append the li element to the unordered list in your HTML
-    list.appendChild(li);
+//     //Append the li element to the unordered list in your HTML
+//     list.appendChild(li);
 
-    //Add an event listener to the delete button that removes the li element when clicked
-    deleteButton.addEventListener('click', () => {
-        list.removeChild(li);
-        clearInput();
-    });
+//     //Add an event listener to the delete button that removes the li element when clicked
+//     deleteButton.addEventListener('click', () => {
+//         list.removeChild(li);
+//         clearInput();
+//     });
 
-    //Send the focus to the input element
-    input.focus();
-    //Change the input value to nothing or the empty string to clean up the interface for the user
-    input.value = '';
+//     //Send the focus to the input element
+//     input.focus();
+//     //Change the input value to nothing or the empty string to clean up the interface for the user
+//     input.value = '';
 
- } else {
-    input.focus();
- }
- //Clear input
- clearInput();
-});
+//  } else {
+//     input.focus();
+//  }
+//  //Clear input
+//  clearInput();
+// });
 
-function clearInput() {
-       //Send the focus to the input element
-       input.focus();
-       //Change the input value to nothing or the empty string to clean up the interface for the user
-       input.value = '';
-}
+// function clearInput() {
+//        //Send the focus to the input element
+//        input.focus();
+//        //Change the input value to nothing or the empty string to clean up the interface for the user
+//        input.value = '';
+// }
 
 // Week 3
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
 // Step 3
 let chaptersArray = getChapterList() || [];
 
