5980a25ed14d23a35a2a32edb4657c99c64e290f
van20020 <van20020@byui.edu>
2024-06-25 11:14:38 -0600

    push json

--- scripts/links.js
+++ scripts/links.js
@@ -1,12 +1,30 @@
 const baseURL = "https://van20020.github.io/wdd230/";
 const linksURL = "https://van20020.github.io/wdd230/data/links.json";
+const allWeeks = document.querySelector("#allWeeks");
 
 async function getLinks() {
-    const response = await fetch(linksURL);
-    const data = await response.json();
-    displayLinks(data);
-  }
-  
-  getLinks();
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  displayLinks(data.weeks);
+}
+
+const displayLinks = (weeks) => {
+  weeks.forEach((week) => {
+    let card = document.createElement("section");
+    let weeks = document.createElement("p");
+    let links = document.createElement("p");
+
+    weeks.textContent = `${week.weeks}`;
+    links.textContent = `${week.links}`;
+
+    card.appendChild(weeks);
+    card.appendChild(links);
+
+    allWeeks.appendChild(card);
+  });
+};
+
+getLinks();
+
 
   
