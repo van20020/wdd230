a3d17e388eea84bbb575b6218698f0b67cbc593a
van20020 <van20020@byui.edu>
2024-06-27 11:00:38 -0600

    changed json :

--- scripts/links.js
+++ scripts/links.js
@@ -1,27 +1,45 @@
 const baseURL = "https://van20020.github.io/wdd230/";
 const linksURL = "https://van20020.github.io/wdd230/data/links.json";
-const allWeeks = document.querySelector("#allWeeks");
 
 async function getLinks() {
-  const response = await fetch(linksURL);
-  const data = await response.json();
-  displayLinks(data.Weeks);
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.weeks);
 }
 
-const displayLinks = (Weeks) => {
-  Weeks.forEach((week) => {
-    let card = document.createElement("section");
-    let weeks = document.createElement("p");
-    let links = document.createElement("p");
-
-    weeks.textContent = `${week.week}`;
-    links.textContent = `${week.links}`;
-
-    card.appendChild(weeks);
-    card.appendChild(links);
-
-    allWeeks.appendChild(card);
-  });
-};
-
 getLinks();
+
+
+function displayLinks(weeks) {
+  const container = document.querySelector('#allWeeks');
+
+  weeks.forEach(week => {
+    // Create the week heading
+    const weekHeader = document.createElement('h2');
+    weekHeader.textContent = week.week;
+    container.appendChild(weekHeader);
+
+    // Create the list of activities
+    const ul = document.createElement('ul');
+
+    week.links.forEach((link, index) => {
+      const li = document.createElement('li');
+      li.style.display = "inline"; // Display list items inline
+
+      const a = document.createElement('a');
+      a.href = `${baseURL}${link.url}`;
+      a.textContent = link.title;
+      li.appendChild(a);
+
+      ul.appendChild(li);
+
+      // Add a pipe separator except for the last link
+      if (index < week.links.length - 1) {
+        const separator = document.createTextNode(" | ");
+        ul.appendChild(separator);
+      }
+    });
+
+    container.appendChild(ul);
+  });
+}
